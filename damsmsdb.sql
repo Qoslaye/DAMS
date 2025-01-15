794c5f6e30384d1912b4bce2998ecc38c838c38c
Hassan Ali <107174931+Qoslaye@users.noreply.github.com>
2025-01-15 13:13:44 +0300

    Update damsmsdb.sql

--- damsmsdb.sql
+++ damsmsdb.sql
@@ -1,28 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 11, 2022 at 07:08 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `damsmsdb`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `tblappointment`
 --
@@ -49,14 +32,7 @@ CREATE TABLE `tblappointment` (
 --
 
 INSERT INTO `tblappointment` (`ID`, `AppointmentNumber`, `Name`, `MobileNumber`, `Email`, `AppointmentDate`, `AppointmentTime`, `Specialization`, `Doctor`, `Message`, `ApplyDate`, `Remark`, `Status`, `UpdationDate`) VALUES
-(1, 141561395, 'Rajesh Kaur', 989, 'raj@gmail.com', '2022-11-12', '12:41:00', '2', 2, 'Thanks', '2022-11-10 06:11:50', 'Cancelled due to incorrect mobile number', 'Cancelled', '2022-11-10 12:40:35'),
-(2, 499219152, 'Mukesh Yadav', 7977797979, 'mukesh@gmail.com', '2022-11-13', '12:30:00', '2', 2, 'bmnbmngfugwakJDiowhfdgr', '2022-11-10 07:08:58', 'Your appointment has been approved, kindly came at mention time', 'Approved', '2022-11-10 12:34:41'),
-(3, 485109480, 'Tina Yadav', 4654564464, 'tina@gmail.com', '2022-11-11', '13:00:00', '1', 1, 'bjnbjh', '2022-11-10 12:08:51', 'Appointment request has been approved', 'Approved', '2022-11-10 14:32:29'),
-(4, 611388102, 'Jyoti', 7897987987, 'jyoti@gmail.com', '2022-11-12', '02:00:00', '1', 1, 'Thanks', '2022-11-10 14:31:17', NULL, NULL, NULL),
-(5, 607441873, 'Anuj kumar', 1425362514, 'anujkkk@hmak.com', '2022-11-16', '20:50:00', '1', 1, 'NA', '2022-11-11 01:19:37', NULL, NULL, NULL),
-(6, 667282012, 'Rahul', 1425251414, 'rk@gmail.com', '2022-11-15', '18:31:00', '2', 2, 'NA', '2022-11-11 01:48:52', 'Approved', 'Approved', '2022-11-11 07:24:25'),
-(7, 599829368, 'Anita', 4563214563, 'anta@test.com', '2022-11-25', '15:20:00', '2', 2, 'NA', '2022-11-11 01:49:54', NULL, NULL, NULL),
-(8, 940019123, 'Amit Kumar', 1425362514, 'amitkr123@test.com', '2022-11-15', '12:30:00', '13', 4, 'NA', '2022-11-11 01:56:17', 'Your appointment has been approved.', 'Approved', '2022-11-11 01:56:55');
+(1, 612567877, 'dhagacade', 989, 'dhagacade@gmail.com', '2022-11-12', '12:41:00', '2', 2, 'Thanks', '2022-11-10 06:11:50', 'Cancelled due to incorrect mobile number', 'Cancelled', '2022-11-10 12:40:35');
 
 -- --------------------------------------------------------
 
@@ -79,10 +55,7 @@ CREATE TABLE `tbldoctor` (
 --
 
 INSERT INTO `tbldoctor` (`ID`, `FullName`, `MobileNumber`, `Email`, `Specialization`, `Password`, `CreationDate`) VALUES
-(1, 'Dr. Anusakha Singh', 9787979798, 'anu@gmail.com', '1', 'f925916e2754e5e03f75dd58a5733251', '2022-11-09 15:01:11'),
-(2, 'Dr. Pradeep Chauhan', 6464654646, 'pra@gmail.com', '2', '202cb962ac59075b964b07152d234b70', '2022-11-09 15:01:59'),
-(3, 'Garima Singh', 14253625, 'gs123@test.com', '7', 'f925916e2754e5e03f75dd58a5733251', '2022-11-11 01:28:44'),
-(4, 'Shiv Kumar Singh', 1231231230, 'skmr123@test.com', '4', 'f925916e2754e5e03f75dd58a5733251', '2022-11-11 01:54:44');
+(1, 'Dr Hassan Ali Jimale', 9787979798, 'qoslaye@gmail.com', '1', 'f925916e2754e5e03f75dd58a5733251', '2022-11-09 15:01:11');
 
 -- --------------------------------------------------------
 
@@ -126,19 +99,12 @@ CREATE TABLE `tblspecialization` (
 --
 
 INSERT INTO `tblspecialization` (`ID`, `Specialization`, `CreationDate`) VALUES
-(1, 'Orthopedics', '2022-11-09 14:22:33'),
-(2, 'Internal Medicine', '2022-11-09 14:23:42'),
-(3, 'Obstetrics and Gynecology', '2022-11-09 14:24:14'),
-(4, 'Dermatology', '2022-11-09 14:24:42'),
-(5, 'Pediatrics', '2022-11-09 14:25:06'),
-(6, 'Radiology', '2022-11-09 14:25:31'),
-(7, 'General Surgery', '2022-11-09 14:25:52'),
-(8, 'Ophthalmology', '2022-11-09 14:27:18'),
-(9, 'Family Medicine', '2022-11-09 14:27:52'),
-(10, 'Chest Medicine', '2022-11-09 14:28:32'),
-(11, 'Anesthesia', '2022-11-09 14:29:12'),
-(12, 'Pathology', '2022-11-09 14:29:51'),
-(13, 'ENT', '2022-11-09 14:30:13');
+(1, 'Cudarada Caruurta', '2022-11-09 14:22:33'),
+(2, 'cudurada lafaha  ', '2022-11-09 14:23:42'),
+(3, 'cudurada maskaxda ', '2022-11-09 14:24:14'),
+(4, 'cudurada haweenka', '2022-11-09 14:24:42'),
+(5, 'cudurada ragga', '2022-11-09 14:25:06'),
+(6, 'ENT', '2022-11-09 14:30:13');
 
 --
 -- Indexes for dumped tables
